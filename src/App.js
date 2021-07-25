e602a12592341fe07cfc2893e6605de5dca673c6
Felipe Magrassi <felipe.1magrassi@gmail.com>
2021-07-25 01:16:12 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import ShopList from "./components/ShopList";
+import Cart from "./components/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <header className="w-full ">
+          <nav className="w-full flex justify-between p-7">
+            <div className="flex-grow font-semibold text-5xl text-center">
+              <Link to="/">Hiring-Coders</Link>
+            </div>
+            <Link to="/cart">Loja</Link>
+          </nav>
+        </header>
+        <Switch>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/">
+            <ShopList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/ShopList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ShopList() {
+  return (
+    <div className="">
+      <h1> ShopList </h1>
+    </div>
+  );
+}
